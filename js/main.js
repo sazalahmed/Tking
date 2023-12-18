f6d08654cc995976d1ddab518d67d15a3b1934e7
freelancersazal <freelancersazal@gmail.com>
2023-12-18 18:21:54 +0600

    home 3 added

--- js/main.js
+++ js/main.js
@@ -285,4 +285,43 @@ $(function () {
         ]
     });
 
+
+    //======instagram slider====== 
+    $('.instagram_slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        dots: false,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
 });
